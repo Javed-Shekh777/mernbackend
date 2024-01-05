094c27f706d43681e97360045f1980ee01af038a
Javed-Shekh777 <144610278+Javed-Shekh777@users.noreply.github.com>
2024-01-05 10:37:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+// const dotenv = require("dotenv");
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// require("./db/conn.js");
+// const router = require("./Routes/router.js");
+// const PORT = process.env.PORT || 8080
+// dotenv.config();
+
 const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 require("./db/conn.js");
+
 const router = require("./Routes/router.js");
 const PORT = process.env.PORT || 8080
-dotenv.config();
 
 app.use(cors());
 app.use(express.json());

da5facddaf768da94f5a3ad5ebf57f2aee21d202
Javed-Shekh777 <144610278+Javed-Shekh777@users.noreply.github.com>
2024-01-05 10:41:03 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 const DB = process.env.MONGO_URI;
 mongoose.connect(DB).then(()=>{
     console.log("Database Connected..");
-}).atch((err)=>{
+}).catch((err)=>{
     console.log("Database not connected...",err);
 })
